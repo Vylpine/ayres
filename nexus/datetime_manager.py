14d566dd152eebcfc3b9091d14bf97d3ad42c9fe
Vylpine <joe.ko.hobbs@gmail.com>
2026-03-28 23:22:52 +0000

    added empty memory_manager

--- nexus/datetime_manager.py
+++ nexus/datetime_manager.py
@@ -9,12 +9,27 @@ def save_event(name, time, value):
         json.dump(eventdata, file, indent=4) # as a .json file
 
 def check_events():
-    milliseconds = int(time()) # milliseconds since last epoch
-    milliseconds -= 86400 # minus one day
+    seconds = int(time()) # seconds since last epoch
+    seconds -= 86400 # minus one day
     for f in os.listdir("./events/"):
         f = f[:-5]
-        if (int(f) >= milliseconds): # consider an event within one day soon
+        if (int(f) >= seconds): # consider an event within one day soon
             with open(f"./events/{f}.json", "r") as file:
                 event = json.load(file)
                 os.remove(f"./events/{f}.json")
                 return event
+
+def save_alarm(name, time):
+    with open(f"./alarms/{time}.json", "w") as file:
+        alarmdata = {"name": name} #           stores name of alarm as a 
+        json.dump(alarmdata, file, indent=4) # .json file
+
+def check_alarms():
+    seconds = int(time()) # seconds since last epoch
+    for f in os.listdir("./alarms/"):
+        f = f[:-5]
+        if (int(f) >= seconds): # if an alarm is set for now, detect it
+            with open(f"./alarms/{f}.json", "r") as file:
+                alarm = json.load(file)
+                os.remove(f"./alarms/{f}.json")
+                return alarm
