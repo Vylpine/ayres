54efe0b7712a83ede223e66871825aa4b9f5a79b
Vylpine <joe.ko.hobbs@gmail.com>
2026-09-06 21:49:15 +0100

    modified start script

--- start.sh
+++ start.sh
@@ -2,21 +2,33 @@
 
 source ./venv/bin/activate
 
-echo Starting core service..
+echo Starting core.service...
 
 python ./core.py &
 
 sleep 1
 
-echo Starting spotify_service..
+echo Starting spotify.service...
 
 python ./spotify_service.py &
 
 sleep 1
 
-echo Starting timer service...
+echo Starting timer.service...
 
 python ./time_service.py &
+
+sleep 1
+
+echo Starting audio_input.service...
+
+python ./audio_input.py &
+
+sleep 1
+
+echo starting voice_detection.service...
+
+python ./voice-detection.py &
 
 sleep 1
 
